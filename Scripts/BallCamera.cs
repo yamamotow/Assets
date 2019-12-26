205193dd3fc02c02321b6b41789ad991ef52006e
Ichiro Ikari <ikaring.w.steve@gmail.com>
2019-12-26 21:56:41 +0900

    second commit

---
+++ Scripts/BallCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCamera : MonoBehaviour
+{
+
+    public Transform tr;
+
+    public Vector3 posOffset = Vector3.zero;
+    public Vector3 angOffcet = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = tr.position + posOffset;
+    }
+}
