b613bd6a8e5e6b582c795af16e838c2b9ad5cffa
wataru <wataru-yamamoto@hiroshima-u.ac.jp>
2020-01-26 15:07:02 +0900

    マテリアル，プレハブを追加　球をランダムに生成するプログラムを追加

---
+++ Scripts/Generator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+
+    public GameObject[] spheres;
+    public Material[] materials;
+    float interval;
+    //Vector2 rb_Pos, rs_Pos, bb_Pos, bs_Pos;
+
+    // Use this for initialization
+    void Start()
+    {
+        //生成間隔の設定
+        interval = Random.Range(4f, 6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > interval)
+        {
+            int rand = Random.Range(1, 3);
+            float y = Random.Range(-6f, 6f);
+            for (int i = 0; i < 3; i++){
+                Instantiate(spheres[(rand + i) % 3], new Vector3(-5 + i * 5, 1, y), Quaternion.identity);
+            }
+            interval += Random.Range(4f, 6f);
+        }
+    }
+}
